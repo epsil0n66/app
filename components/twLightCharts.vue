498f5fc5465ed3bf4d1f41634622490b99d90744
Alexander Slanina <ale.slanina@gmail.com>
2022-12-07 11:42:36 +0500

    Обновление графиков

--- components/twLightCharts.vue
+++ components/twLightCharts.vue
@@ -8,7 +8,7 @@
           v-bind="attrs"
           v-on="on"
         >
-          {{ activeButton }}
+          {{ currentInterval.time + currentInterval.name }}
         </v-btn>
       </template>
       <v-list
@@ -17,7 +17,7 @@
         link
       >
         <v-list-item
-          @click="applyInterval(item)"
+          @click="(currentInterval = item)"
         >
           <v-list-item-title>{{ item.time }} {{ item.name }}</v-list-item-title>
         </v-list-item>
@@ -31,7 +31,7 @@
           v-bind="attrs"
           v-on="on"
         >
-          {{ activeSeriesButton }}
+          {{ currentSeries.name }}
         </v-btn>
       </template>
       <v-list
@@ -40,12 +40,17 @@
         link
       >
         <v-list-item
-          @click="applySeries(item)"
+          @click="(currentSeries = item)"
         >
           <v-list-item-title>{{ item.name }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
+    <v-btn
+      @click="takeScreenshot"
+    >
+      Take screenshot
+    </v-btn>
     <div
       :id="chartID"
       class="twchart"
@@ -67,90 +72,116 @@ export default {
     return {
       chartID: 'twChart',
       intervalMenu: [],
-      activeButton: '1 m',
-      activeSeriesButton: 'Candlestick',
+      currentInterval: { time: '1', name: 'm' },
       seriesMenu: [
-        { name: 'Candlestick' },
-        { name: 'Bar' },
-        { name: 'Line' }
-      ]
+        { name: 'Candlestick', id: 0 },
+        { name: 'Bar', id: 1 },
+        { name: 'Line', id: 2 }
+      ],
+      currentSeries: { name: 'Candlestick', id: 0 },
+      chartData: null,
+      lineData: null
+    }
+  },
+  watch: {
+    currentInterval () {
+      this.loadData().then(() => {
+        this.applySeries()
+      })
+    },
+    currentSeries () {
+      this.applySeries()
     }
   },
   mounted () {
-    chart = createChart(this.chartID,
-      {
-        rightPriceScale: { visible: true },
-        leftPriceScale: { visible: true },
-        width: 600,
-        height: 400,
-        crosshair: { mode: 0 },
-        timeScale: {
-          timeVisible: true,
-          secondsVisible: false
-        }
-      })
     instance = this.$axios.create()
     delete instance.defaults.headers.Authorization
-    candlestickSeries = chart.addCandlestickSeries()
-    barSeries = chart.addBarSeries()
-    lineSeries = chart.addLineSeries()
     instance('http://arobots.evospb.ru/api/timeframes')
       .then((res) => {
         this.intervalMenu = res.data
       })
-    this.applyInterval({ time: '1', name: 'm' })
-    chart.timeScale().fitContent()
-    const socket = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1m')
-    socket.onmessage = function (event) {
-      const a = JSON.parse(event.data).k
-      candlestickSeries.update({ time: a.t / 1000, open: parseFloat(a.o), high: parseFloat(a.h), low: parseFloat(a.l), close: parseFloat(a.c) })
-      barSeries.update({ time: a.t / 1000, open: parseFloat(a.o), high: parseFloat(a.h), low: parseFloat(a.l), close: parseFloat(a.c) })
-      lineSeries.update({ time: a.t / 1000, value: parseFloat(a.c) })
-    }
-    const maxPriceLine = {
-      price: 1280,
-      color: 'red',
-      lineWidth: 2,
-      lineStyle: 2, // LineStyle.Dashed
-      axisLabelVisible: true,
-      title: 'Price'
-    }
-    candlestickSeries.createPriceLine(maxPriceLine)
-    barSeries.createPriceLine(maxPriceLine)
-    lineSeries.createPriceLine(maxPriceLine)
-    const markers = [
-      {
-        time: Date.now() / 1000,
-        position: 'aboveBar',
-        color: 'green',
-        shape: 'circle',
-        text: 'Marker'
-      }
-    ]
-    candlestickSeries.setMarkers(markers)
-    barSeries.setMarkers(markers)
-    lineSeries.setMarkers(markers)
+    this.loadData().then(() => {
+      this.applySeries()
+    })
+    // const socket = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1m')
+    // socket.onmessage = function (event) {
+    //   const a = JSON.parse(event.data).k
+    //   candlestickSeries.update({ time: a.t / 1000, open: parseFloat(a.o), high: parseFloat(a.h), low: parseFloat(a.l), close: parseFloat(a.c) })
+    //   barSeries.update({ time: a.t / 1000, open: parseFloat(a.o), high: parseFloat(a.h), low: parseFloat(a.l), close: parseFloat(a.c) })
+    //   lineSeries.update({ time: a.t / 1000, value: parseFloat(a.c) })
+    // }
+    // const maxPriceLine = {
+    //   price: 1280,
+    //   color: 'red',
+    //   lineWidth: 2,
+    //   lineStyle: 2, // LineStyle.Dashed
+    //   axisLabelVisible: true,
+    //   title: 'Price'
+    // }
+    // candlestickSeries.createPriceLine(maxPriceLine)
+    // barSeries.createPriceLine(maxPriceLine)
+    // lineSeries.createPriceLine(maxPriceLine)
+    // const markers = [
+    //   {
+    //     time: Date.now() / 1000,
+    //     position: 'aboveBar',
+    //     color: 'green',
+    //     shape: 'circle',
+    //     text: 'Marker'
+    //   }
+    // ]
+    // candlestickSeries.setMarkers(markers)
+    // barSeries.setMarkers(markers)
+    // lineSeries.setMarkers(markers)
   },
   methods: {
-    applyInterval (interval) {
-      console.log(`${interval.time}${interval.name}`)
-      instance(`https://www.binance.com/api/v3/uiKlines?symbol=ETHBUSD&interval=${interval.time}${interval.name}&limit=1000`)
+    applySeries () {
+      if (chart) {
+        chart.remove()
+      }
+      chart = createChart(this.chartID,
+        {
+          rightPriceScale: { visible: true },
+          leftPriceScale: { visible: true },
+          width: 600,
+          height: 400,
+          crosshair: { mode: 0 },
+          timeScale: {
+            timeVisible: true,
+            secondsVisible: false
+          }
+        })
+      chart.timeScale().fitContent()
+      switch (this.currentSeries.id) {
+        case (0):
+          candlestickSeries = chart.addCandlestickSeries()
+          candlestickSeries.setData(this.chartData)
+          break
+        case (1):
+          barSeries = chart.addBarSeries()
+          barSeries.setData(this.chartData)
+          break
+        case (2):
+          lineSeries = chart.addLineSeries()
+          lineSeries.setData(this.lineData)
+      }
+    },
+    async loadData () {
+      await instance(`https://www.binance.com/api/v3/uiKlines?symbol=ETHBUSD&interval=${this.currentInterval.time}${this.currentInterval.name}&limit=100`)
         .then((res) => {
-          const chartData = res.data.map((a) => {
+          this.chartData = res.data.map((a) => {
             return { time: a[0] / 1000, open: parseFloat(a[1]), high: parseFloat(a[2]), low: parseFloat(a[3]), close: parseFloat(a[4]) }
           })
-          const lineData = res.data.map((a) => {
+          console.log(res)
+          this.lineData = res.data.map((a) => {
             return { time: a[0] / 1000, value: parseFloat(a[4]) }
           })
-          candlestickSeries.setData(chartData)
-          barSeries.setData(chartData)
-          lineSeries.setData(lineData)
         })
-      this.activeButton = interval.time + interval.name
     },
-    applySeries (series) {
-      console.log(series)
-      this.activeSeriesButton = series.name
+    takeScreenshot () {
+      chart.takeScreenshot().toBlob((blob) => {
+        navigator.clipboard.write([new window.ClipboardItem({ 'image/png': blob })])
+      })
     }
   }
 }
