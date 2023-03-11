4b78ef4bcc3834bb52ee38d1e9a6ee61ffd71ee1
ale.slanina <ale.slanina@gmail.com>
2023-03-11 12:18:05 +0500

    Ипотечный калькулятор

--- pages/profile/calculators/index.vue
+++ pages/profile/calculators/index.vue
@@ -1,51 +1,5 @@
 <template>
   <v-app>
-    <div
-      v-if="pageAlert"
-      no-gutters
-      class="d-flex align-center"
-      style="height: 10vh; width: 100vw; background-color: #aaf7a3; position: fixed; z-index: 999; top: 0"
-      :style="[alertType === 'error' ? { 'background-color': '#f94449' } : { 'background-color': '#aaf7a3' }]"
-    >
-      <v-icon
-        v-if="alertType != 'error'"
-        color="#1AC200"
-        class="mr-2"
-        style="margin-left:10%;"
-      >
-        mdi-check-circle-outline
-      </v-icon>
-      <v-icon
-        v-if="alertType === 'error'"
-        color="white"
-        class="mr-2"
-        style="margin-left:10%;"
-      >
-        mdi-alert
-      </v-icon>
-      <p
-        v-if="alertType != 'error'"
-        class="ma-0"
-        style="color:#1AC200;"
-      >
-        Thanks!
-      </p>
-      <p
-        :class="[alertType === 'error' ? 'white--text' : '']"
-        class="pl-6 ma-0"
-      >
-        {{ pageAlertText }}
-      </p>
-      <v-spacer />
-      <v-icon
-        color="white"
-        float="right"
-        style="cursor:pointer; margin-right: 10vw;"
-        @click="pageAlert = false"
-      >
-        mdi-close-circle-outline
-      </v-icon>
-    </div>
     <v-main>
       <div>
         <v-card
@@ -55,29 +9,85 @@
           <v-row
             no-gutters
           >
-            <h1>Treasurer</h1>
+            <h1>Calculators</h1>
             <v-spacer />
-            <v-btn
-              v-for="button in buttons"
-              :key="button.id"
-              :style="[button.active ? {'color': 'primary'} : {'background': '#FFF'}]"
-              height="48px"
-              width="150px"
-              style="border-radius:12px ;"
-              :outlined="!button.active"
-              color="primary"
-              class="mb-4 mx-3"
-              :class="{ 'white--text' : button.active }"
-              @click="toggleQuery(button)"
-            >
-              {{ button.name }}
-            </v-btn>
           </v-row>
-          <stocksTable
-            :hide-footer="true"
-            :stocks-table-data="stocksTableData"
-            :stocks-path="'calculators'"
-          />
+          <v-row>
+            <v-col
+              cols="4"
+            >
+              <h2>Ипотечный калькулятор</h2>
+              <v-slider
+                v-model="mortgageCalc.percentage"
+                class="mt-4"
+                min="3"
+                max="30"
+                step="0.1"
+                :thumb-label="true"
+                thumb-color="green"
+                label="Процент ипотеки"
+              />
+              <v-text-field
+                v-model="mortgageCalc.percentage"
+                suffix="%"
+                hide-details
+                label="Процент ипотеки"
+                outlined
+              />
+              <v-slider
+                v-model="mortgageCalc.total"
+                class="mt-8"
+                min="500000"
+                max="30000000"
+                step="100000"
+                label="Стоимость недвижимости"
+              />
+              <v-text-field
+                v-model="formattedMortgageTotal"
+                hide-details
+                label="Стоимость недвижимости"
+                outlined
+              />
+              <v-slider
+                v-model="mortgageCalc.initialFee"
+                class="mt-8"
+                :min="mortgageCalc.total / 100 * 15"
+                :max="mortgageCalc.total / 100 * 95"
+                step="100000"
+                label="Первоначальный взнос"
+              />
+              <v-text-field
+                v-model="mortgageCalc.initialFee"
+                hide-details
+                label="Первоначальный взнос"
+                outlined
+              />
+              <v-slider
+                v-model="mortgageCalc.duration"
+                class="mt-8"
+                min="1"
+                :max="30"
+                label="Срок кредита"
+              />
+              <v-text-field
+                v-model="mortgageCalc.duration"
+                hide-details
+                label="Срок кредита"
+                outlined
+              />
+            </v-col>
+            <v-spacer />
+            <v-col
+              cols="6"
+            >
+              <p
+                class="mb-2"
+              >
+                Ежемесячный платеж
+              </p>
+              <h1> {{ mortgagePayment }} </h1>
+            </v-col>
+          </v-row>
         </v-card>
       </div>
     </v-main>
@@ -85,71 +95,43 @@
 </template>
 
 <script>
-import stocksTable from '~/components/stocksTable.vue'
-import config from '@/config'
 
 export default {
   name: 'TreasurerFrontendAddedstocks',
-  components: { stocksTable },
   data () {
     return {
-      pageAlert: false,
-      pageAlertText: null,
-      alertType: null,
-      stocksTableData: [],
-      url: null,
-      buttons: [
-        { name: 'Rent', active: true },
-        { name: 'Active', active: false },
-        { name: 'Archive', active: false }
-      ]
+      mortgageCalc: {
+        percentage: 0,
+        total: 0,
+        initialFee: 0,
+        duration: 0
+      },
+      mortgagePayment: 0
+    }
+  },
+  computed: {
+    formattedMortgageTotal () {
+      return new Intl.NumberFormat().format(this.mortgageCalc.total)
+    }
+  },
+  watch: {
+    mortgageCalc: {
+      handler: function (newMortgage) {
+        this.mortgagePayment = ((newMortgage.total - newMortgage.initialFee) * (newMortgage.percentage / 12)) / (1 - (1 + newMortgage.percentage / 12) * (1 - newMortgage.duration * 12))
+        this.mortgagePayment = Math.floor(this.mortgagePayment)
+      },
+      deep: true
     }
   },
   mounted () {
-    this.$axios.get(`${config.apiUrl}/rents?page=1`).then((res) => {
-      this.stocksTableData = res.data.map(el => el.robot)
-    })
-      .catch((e) => {
-        if (e.response.status === 401) {
-          this.$axios.get(`${config.apiUrl}/rents?page=1`).then((res) => {
-            this.stocksTableData = res.data.map(el => el.robot)
-          })
-        }
-      })
+
   },
   methods: {
-    toggleQuery (button) {
-      const index = this.buttons.indexOf(button)
-      this.buttons.forEach((button) => { button.active = false })
-      this.buttons[index].active = true
-      const pages = 1
-      let filterQuery = `page=${pages}&`
-      if (index === 1) {
-        filterQuery += 'active=true'
-      }
-      if (index === 2) {
-        filterQuery += 'archived=true'
-      }
-      this.$axios.get(`${config.apiUrl}/rents?${filterQuery}`).then((res) => {
-        if (res.data) {
-          this.stocksTableData = res.data.map(el => el.robot)
-        } else {
-          this.stocksTableData = null
-        }
-      })
-      console.log(filterQuery)
-    }
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.inputfile {
-width: 0.1px;
-height: 0.1px;
-opacity: 0;
-overflow: hidden;
-position: absolute;
-z-index: -1;
-}
+
 </style>
